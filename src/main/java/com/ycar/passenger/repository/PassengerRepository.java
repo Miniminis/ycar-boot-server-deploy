4889b109967d3be3fbd4d554b6857fae748615b0
minhee.son <44446425+Miniminis@users.noreply.github.com>
2019-10-21 16:40:58 +0900

    [ycar-boot-server] 연차 부트 서버 재배포 v2.4

--- src/main/java/com/ycar/passenger/repository/PassengerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ycar.passenger.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.ycar.passenger.entity.PassengerEntity;
-
-public interface PassengerRepository extends JpaRepository<PassengerEntity, Integer> {
-
-}
